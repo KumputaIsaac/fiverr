992012617cb9bdb119edc48d72c0bca321fad94c
kumputaisaac <kumputaisaac@gmail.com>
2023-05-13 12:46:53 +0100

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./navbar.scss";
+// import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+          {/* </Link> */}
+        </div>
+        <div className="links">
+          <span>Fiver Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          {!currentUser?.isSeller && <span>Become a seller</span>}
+          <button>Join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/gigs/Gigs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gig.scss";
+
+const Gigs = () => {
+  return <div className="gigs">Gigs</div>;
+};
+
+export default Gigs;
